d64471987522edcf1421d395951eac68f3cd8de3
Riddhi Bansal <riddhibansal@Riddhis-MacBook-Air.local>
2023-11-04 10:47:31 +0530

    Initial version of Pokemon list and list item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './pokemon-list-item/pokemon-list-item.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonListItemComponent,
+    PokemonDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
